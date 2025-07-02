e9cbe07f1fe88503fda3387509f90ef5ce1830b0
EuphoricPenguin <euphoricpenguin@protonmail.com>
2025-07-01 21:12:45 -0400

    Fixed basic boilerplate for Blockly

--- src/blockly_blocks.js
+++ src/blockly_blocks.js
@@ -1,59 +1,294 @@
+Blockly.Blocks['pseudowoodo_variable_declare'] = {
+  init: function() {
+    this.appendValueInput('VALUE')
+        .setCheck(null)
+        .appendField('set')
+        .appendField(new Blockly.FieldTextInput('var-name'), 'VAR_NAME')
+        .appendField('as');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('Declare a new variable');
+  }
+};
 
-// Core blocks
+Blockly.Blocks['pseudowoodo_variable_assign'] = {
+  init: function() {
+    this.appendValueInput('VALUE')
+        .setCheck(null)
+        .appendField(new Blockly.FieldTextInput('var-name'), 'VAR_NAME')
+        .appendField('as');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('Assign a value to an existing variable');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_arithmetic'] = {
+  init: function() {
+    this.appendValueInput('A')
+        .setCheck('Number');
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([
+          ['plus', 'plus'],
+          ['minus', 'minus'],
+          ['times', 'times'],
+          ['over', 'over']
+        ]), 'OP');
+    this.appendValueInput('B')
+        .setCheck('Number');
+    this.setInputsInline(true);
+    this.setOutput(true, 'Number');
+    this.setColour(160);
+    this.setTooltip('Perform arithmetic operations');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_comparison'] = {
+  init: function() {
+    this.appendValueInput('A')
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([
+          ['is less than', 'is-less-than'],
+          ['is greater than', 'is-greater-than'],
+          ['is less than or equal to', 'is-less-than-or-equal-to'],
+          ['is greater than or equal to', 'is-greater-than-or-equal-to'],
+          ['is equal to', 'is-equal-to']
+        ]), 'OP');
+    this.appendValueInput('B')
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setOutput(true, 'Boolean');
+    this.setColour(210);
+    this.setTooltip('Compare two values');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_logic'] = {
+  init: function() {
+    this.appendValueInput('A')
+        .setCheck('Boolean');
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([
+          ['and', 'and'],
+          ['or', 'or']
+        ]), 'OP');
+    this.appendValueInput('B')
+        .setCheck('Boolean');
+    this.setInputsInline(true);
+    this.setOutput(true, 'Boolean');
+    this.setColour(210);
+    this.setTooltip('Logical operations');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_not'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('not');
+    this.appendValueInput('BOOL')
+        .setCheck('Boolean');
+    this.setInputsInline(true);
+    this.setOutput(true, 'Boolean');
+    this.setColour(210);
+    this.setTooltip('Logical NOT operation');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_if'] = {
+  init: function() {
+    this.appendValueInput('CONDITION')
+        .setCheck('Boolean')
+        .appendField('if');
+    this.appendStatementInput('THEN')
+        .appendField('call');
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput('action'), 'CALL_NAME');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('Conditional statement');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_if_else'] = {
+  init: function() {
+    this.appendValueInput('CONDITION')
+        .setCheck('Boolean')
+        .appendField('if');
+    this.appendStatementInput('THEN')
+        .appendField('call');
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput('action'), 'THEN_CALL');
+    this.appendStatementInput('ELSE')
+        .appendField('else call');
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput('action'), 'ELSE_CALL');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('Conditional statement with else');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_call'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('call')
+        .appendField(new Blockly.FieldTextInput('action'), 'CALL_NAME');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('Call a labeled section of code');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_label'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(':')
+        .appendField(new Blockly.FieldTextInput('label'), 'LABEL_NAME');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('Label for a section of code');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_array_create'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('set')
+        .appendField(new Blockly.FieldTextInput('array-name'), 'ARRAY_NAME')
+        .appendField('as [');
+    this.appendValueInput('ELEMENTS')
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(']');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('Create a new array');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_array_length'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('length of')
+        .appendField(new Blockly.FieldTextInput('array-name'), 'ARRAY_NAME');
+    this.setOutput(true, 'Number');
+    this.setColour(260);
+    this.setTooltip('Get length of an array');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_array_access'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('at-index')
+        .appendField(new Blockly.FieldNumber(0, 0), 'INDEX')
+        .appendField('of')
+        .appendField(new Blockly.FieldTextInput('array-name'), 'ARRAY_NAME');
+    this.setOutput(true, null);
+    this.setColour(260);
+    this.setTooltip('Access array element by index');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_array_set'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('set-index')
+        .appendField(new Blockly.FieldNumber(0, 0), 'INDEX')
+        .appendField('of')
+        .appendField(new Blockly.FieldTextInput('array-name'), 'ARRAY_NAME')
+        .appendField('as');
+    this.appendValueInput('VALUE')
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('Set array element by index');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_input'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('set')
+        .appendField(new Blockly.FieldTextInput('var-name'), 'VAR_NAME')
+        .appendField('as input');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('Get user input');
+  }
+};
+
+Blockly.Blocks['pseudowoodo_console_log'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField('console-log as');
+    this.appendValueInput('VALUE')
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField('call console-log');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(290);
+    this.setTooltip('Output to console');
+  }
+};
+
 Blockly.Blocks['pseudowoodo_comment'] = {
   init: function() {
     this.appendDummyInput()
         .appendField('rem')
         .appendField(new Blockly.FieldTextInput('comment'), 'COMMENT');
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(160);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
     this.setTooltip('Add a comment');
   }
 };
 
-// [Previous block definitions...]
-
-// Existing generators
-Blockly.JavaScript['pseudowoodo_array_access'] = function(block) {
-  const index = block.getFieldValue('INDEX');
-  const array = block.getFieldValue('ARRAY');
-  return `at-index ${index} of ${array}\n`;
+Blockly.Blocks['pseudowoodo_string'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput('text'), 'TEXT');
+    this.setOutput(true, 'String');
+    this.setColour(160);
+    this.setTooltip('String literal');
+  }
 };
 
-// ======================
-// CODE GENERATION SETUP
-// ======================
-
-// Initialize Blockly code generator
-Blockly.JavaScript = new Blockly.Generator('PseudoWoodo');
-
-// Add workspace change handler
-function setupBlocklyWorkspace(workspace) {
-  workspace.addChangeListener(function(event) {
-    if (!event.isUiEvent && !event.recordUndo) {
-      updateCodeFromBlocks();
-    }
-  });
-}
-
-// Update text editor from blocks
-function updateCodeFromBlocks() {
-  try {
-    const code = Blockly.JavaScript.workspaceToCode(window.blocklyWorkspace);
-    document.getElementById('code').value = code;
-    if (typeof applyHighlighting === 'function') {
-      applyHighlighting();
-    }
-  } catch (e) {
-    console.error('Error generating code:', e);
+Blockly.Blocks['pseudowoodo_number'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldNumber(0), 'NUMBER');
+    this.setOutput(true, 'Number');
+    this.setColour(160);
+    this.setTooltip('Number literal');
   }
-}
+};
 
-// Initialize when Blockly is loaded
-if (typeof Blockly !== 'undefined') {
-  Blockly.JavaScript.addReservedWords('rem,set,call,if,else,while,for');
-  if (window.blocklyWorkspace) {
-    setupBlocklyWorkspace(window.blocklyWorkspace);
+Blockly.Blocks['pseudowoodo_boolean'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([
+          ['true', 'true'],
+          ['false', 'false']
+        ]), 'BOOL');
+    this.setOutput(true, 'Boolean');
+    this.setColour(210);
+    this.setTooltip('Boolean value');
   }
-}
+};
---
+++ src/editor.js
@@ -0,0 +1,226 @@
+let interpreter = null;
+let blocklyWorkspace = null;
+
+function switchToTextEditor() {
+    document.querySelectorAll('.tab-button').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    document.querySelector('.tab-button:nth-child(1)').classList.add('active');
+    
+    document.getElementById('text-editor').classList.remove('hidden');
+    document.getElementById('blockly-editor').classList.add('hidden');
+    document.getElementById('code').focus();
+}
+
+function switchToBlockly() {
+    document.querySelectorAll('.tab-button').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    document.querySelector('.tab-button:nth-child(2)').classList.add('active');
+    
+    document.getElementById('blockly-editor').classList.remove('hidden');
+    document.getElementById('text-editor').classList.add('hidden');
+    
+    if (!blocklyWorkspace) {
+        blocklyWorkspace = Blockly.inject('blocklyDiv', {
+            toolbox: {
+                "kind": "categoryToolbox",
+                "contents": [
+                    {
+                        "kind": "category",
+                        "name": "Variables",
+                        "colour": 230,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_variable_declare"},
+                            {"kind": "block", "type": "pseudowoodo_variable_assign"},
+                            {"kind": "block", "type": "pseudowoodo_input"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "Logic",
+                        "colour": 210,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_logic"},
+                            {"kind": "block", "type": "pseudowoodo_not"},
+                            {"kind": "block", "type": "pseudowoodo_comparison"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "Math",
+                        "colour": 160,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_arithmetic"},
+                            {"kind": "block", "type": "pseudowoodo_number"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "Text",
+                        "colour": 160,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_string"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "Control",
+                        "colour": 120,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_if"},
+                            {"kind": "block", "type": "pseudowoodo_if_else"},
+                            {"kind": "block", "type": "pseudowoodo_call"},
+                            {"kind": "block", "type": "pseudowoodo_label"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "Arrays",
+                        "colour": 260,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_array_create"},
+                            {"kind": "block", "type": "pseudowoodo_array_length"},
+                            {"kind": "block", "type": "pseudowoodo_array_access"},
+                            {"kind": "block", "type": "pseudowoodo_array_set"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "I/O",
+                        "colour": 290,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_console_log"}
+                        ]
+                    },
+                    {
+                        "kind": "category",
+                        "name": "Comments",
+                        "colour": 60,
+                        "contents": [
+                            {"kind": "block", "type": "pseudowoodo_comment"}
+                        ]
+                    }
+                ]
+            },
+            grid: {
+                spacing: 20,
+                length: 3,
+                colour: '#333',
+                snap: true
+            },
+            zoom: {
+                controls: true,
+                wheel: true,
+                startScale: 1.0,
+                maxScale: 3,
+                minScale: 0.3,
+                scaleSpeed: 1.2
+            }
+        });
+        
+        // Add a default block if workspace is empty
+        const startBlock = blocklyWorkspace.newBlock('pseudowoodo_console_log');
+        startBlock.initSvg();
+        startBlock.render();
+        startBlock.setDeletable(false);
+    }
+}
+
+function applyHighlighting() {
+    const code = document.getElementById('code');
+    const highlight = document.getElementById('highlight');
+    const text = code.value;
+
+    const highlighted = text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/(set|call|if|rem|as|else)\b/g, '<span class="keyword">$1</span>')
+        .replace(/'([^']*)'/g, '<span class="string">\'$1\'</span>')
+        .replace(/(^| )rem .*/gm, '<span class="comment">$&</span>')
+        .replace(/:(\w+)/g, '<span class="label">:$1</span>')
+        .replace(/(console-log|console-clear|timeout)\b/g, '<span class="function">$1</span>');
+
+    highlight.innerHTML = highlighted;
+    highlight.scrollTop = code.scrollTop;
+    highlight.scrollLeft = code.scrollLeft;
+}
+
+function initEditor() {
+    const code = document.getElementById('code');
+    code.addEventListener('input', applyHighlighting);
+    code.addEventListener('scroll', () => {
+        const highlight = document.getElementById('highlight');
+        highlight.scrollTop = code.scrollTop;
+        highlight.scrollLeft = code.scrollLeft;
+    });
+    applyHighlighting();
+}
+
+function runCode() {
+    let code;
+    const output = document.getElementById('output');
+    output.innerHTML = '';
+    
+    if (document.getElementById('text-editor').classList.contains('hidden')) {
+        code = Blockly.JavaScript.workspaceToCode(blocklyWorkspace);
+    } else {
+        code = document.getElementById('code').value;
+    }
+
+    const logo = document.getElementById('logo1');
+    logo.classList.add('spin');
+    logo.addEventListener('animationend', () => {
+        logo.classList.remove('spin');
+    }, { once: true });
+
+    if (interpreter) interpreter.stop();
+
+    interpreter = new PseudoWoodoInterpreter(
+        message => {
+            if (message === null) {
+                output.innerHTML = '';
+            } else {
+                const div = document.createElement('div');
+                if (message.isError) {
+                    div.className = 'error';
+                }
+                div.textContent = message.message || message;
+                output.appendChild(div);
+                output.scrollTop = output.scrollHeight;
+            }
+        }
+    );
+
+    interpreter.execute(code).catch(e => {
+        const errorDiv = document.createElement('div');
+        errorDiv.className = 'error';
+        errorDiv.textContent = `Error: ${e.message}`;
+        output.appendChild(errorDiv);
+        output.scrollTop = output.scrollHeight;
+    });
+}
+
+document.getElementById('user-input').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        sendInput();
+    }
+});
+
+function sendInput() {
+    const inputField = document.getElementById('user-input');
+    const inputValue = inputField.value.trim();
+
+    if (interpreter && inputValue) {
+        interpreter.provideInput(inputValue);
+        inputField.value = '';
+    }
+}
+
+// Initialize on load
+window.addEventListener('load', function() {
+    initEditor();
+    switchToBlockly(); // Initialize Blockly
+    switchToTextEditor(); // Default to text editor
+});
